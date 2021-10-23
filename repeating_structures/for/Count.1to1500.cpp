92ea05b337c7a5ee6d2cb9c529ef4ff4646492f4
Gustavo <gustavopaulody@gmail.com>
2021-10-22 22:27:11 -0300

    re-organazing folders

---
+++ repeating_structures/for/Count.1to1500.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int counter;
+
+    for(counter = 1; counter <= 1500; counter++)
+    {
+        printf("\n %d", counter);
+    }
+
+    getch();
+
+    return 0;
+}

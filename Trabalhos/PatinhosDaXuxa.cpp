741fe0232dabd75d165eb223d1a7c0fc832515e5
hunteradv <gustavopaulody@gmail.com>
2021-11-28 12:45:21 -0300

    adicionando if no PatinhosDaXuxa

--- Trabalhos/PatinhosDaXuxa.cpp
+++ Trabalhos/PatinhosDaXuxa.cpp
@@ -7,9 +7,16 @@ int main ()
     printf ("\nDigite quantos patinhos deseja que a mamae pata tenha: ");
     scanf ("%d", &duck);
 
-    for (contador = duck; contador > 1; contador--)
+    for (contador = duck; contador > 0; contador--)
     {
-        printf ( "\n %d patinhos foram passear. Alem das montanhas, para brincar. A mamae gritou: Qua, qua, qua, qua. Mas os %d patinhos voltaram de la.", contador, contador - 1);
+        if (contador > 1)
+        {
+            printf ( "\n %d patinhos foram passear. Alem das montanhas, para brincar. A mamae gritou: Qua, qua, qua, qua. Mas os %d patinhos voltaram de la.", contador, contador - 1);
+        }
+        else
+        {
+            printf ( "\n %d patinho foi passear. Alem das montanhas, para brincar. A mamae gritou: Qua, qua, qua, qua. Mas nenhum patinho voltou de la.", contador, contador - 1);
+        }
     }
 
     printf ("\n \nA mamae patinha foi procurar. Alem das montanhas. Na beira do mar. A mamae gritou: Qua, qua, qua, qua. E os %d patinhos voltaram de la.", duck);
